0b12f8886a45e544bcf94514e69426b9d7d610cd
M Newby <newbym2@rpi.edu>
2014-02-07 21:19:49 -0500

    Added Source for Plugin and began testing

---
+++ Source/BOOM.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+ 
+using UnityEngine;
+ 
+ 
+/// <summary>
+/// My first part!
+/// </summary>
+public class TestModule : PartModule
+{
+    /// <summary>
+    /// Called when the part is started by Unity.
+    /// </summary>
+    public override void OnStart(StartState state)
+    {
+        // Add stuff to the log
+        print("something silly to the log");
+    }
+}
